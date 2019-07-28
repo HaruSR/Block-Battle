587e27f67975b6780b3d87d1dee02a93bd0ab5e4
RxOxHaru <runking.orange@gmail.com>
2019-07-28 18:36:50 +0900

    [add]DragObject：ドラッグでGameObjectを操作させるスクリプトを追加

---
+++ Block Battle/Assets/Script/DragObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+///<summary>
+///ドラッグでGameObjectを操作させるオブジェクト
+///操作対象のGameObjectにこのスクリプトをアタッチ
+///<summary>
+public class DragObject : MonoBehaviour, IDragHandler
+{
+    public RectTransform m_rectTransform = null;
+    
+    private void Reset()
+    {
+        m_rectTransform = GetComponent<RectTransform>();
+    }
+    
+    public void OnDrag( PointerEventData e )
+    {
+        m_rectTransform.position += new Vector3( e.delta.x, e.delta.y, 0f );
+    }
+}
+
