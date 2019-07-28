3368e303261970f22677541d933cfeebf8af40f9
JBS\ttokunaga <ttokunaga@jbs.com>
2019-07-28 16:25:55 +0900

    [Add] Title StartButton Evnet etc

---
+++ Block Battle/Assets/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Title_Screen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Title_Screen.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Block Battle/Assets/Script/StartButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    public GameObject Title_Screen;
+    public GameObject Game_Screen;
+
+    void Awake() {
+        Debug.Log("a");
+        GameObject buttonA = GameObject.Find("Tile_User_Name/Button");
+        buttonA.SetActive(false);
+        this.gameObject.GetComponent<Button>().transform.position = new Vector3(this.gameObject.GetComponent<Button>().transform.position.x + 30,this.gameObject.GetComponent<Button>().transform.position.y,0);
+
+    }
+
+    public void SwitchScreen() {
+        Game_Screen.SetActive(true);
+        Title_Screen.SetActive(false);
+    }
+}
