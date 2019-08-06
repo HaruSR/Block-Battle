79bf67d2fc5b02b81e254337feb5d003c071a64b
RxOxHaru <runking.orange@gmail.com>
2019-08-06 23:09:44 +0900

    [add]ImageのSpriteを動的に変更するメソッドを追加

---
+++ Block Battle/Assets/Script/ExchangeImage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExchangeImage : MonoBehaviour
+{
+    float changeTime;
+        public Sprite Image1;
+        public Sprite Image2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        changeTime += Time.deltaTime;
+
+        if(changeTime < 0.5){
+            gameObject.GetComponent<Image>().sprite = Image1;
+            return;
+        }
+        else if(changeTime >= 0.5 && changeTime < 1){
+            gameObject.GetComponent<Image>().sprite = Image2;
+           return;
+        }
+            changeTime = 0;
+            return;
+
+    }
+}
